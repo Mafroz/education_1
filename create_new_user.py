f9a94d92de394a2d5eb7d39aadab98d603de8b80
Mafroz <maksim.zasetskiy@gmail.com>
2019-11-12 17:35:23 +0300

    какие-то изменения

--- create_new_user.py
+++ create_new_user.py
@@ -7,13 +7,17 @@ from application import Application
 @pytest.fixture()
 def app(request):
     fixture = Application()
+    fixture.login(username="administrator", password="5ecr3t")
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_create_new_user(app):
-    app.login(username="administrator", password="5ecr3t")
+    app.open_users_page
+    app.create_new_user
+
+
     #app.navigate_to_submenu_element(elementid=2, submenu_title="Пользователи")
-    app.new_user_name()
+    #app.new_user_name()
     #app.click_on_button_in_toolbar_by_id(0)
     #time.sleep(10)
---
+++ objects_farm.py
@@ -0,0 +1,9 @@
+from application import Application
+
+
+class ObjectsFarm:
+    #def __init__(self):
+
+
+    def open_users_page(self):
+
